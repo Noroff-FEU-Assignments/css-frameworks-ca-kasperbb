2a6232a2228200894ceb7a096b7df9b5cad02f0b
kasperbb <kasperbb98@gmail.com>
2021-09-19 17:48:49 +0200

    Updates contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -1,27 +1,45 @@
 import { useState } from 'react'
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
+import InputGroup from 'react-bootstrap/InputGroup'
+import FormControl from 'react-bootstrap/FormControl'
 
 const ContactForm = () => {
 	const [checked, setChecked] = useState(true)
 
 	return (
-		<Form className="contact-form p-4 bg-light">
+		<Form className="contact-form p-4 bg-light rounded">
 			<Form.Group className="mb-3" controlId="name">
 				<Form.Label>Name</Form.Label>
-				<Form.Control type="text" className="border-0" />
+				<Form.Control type="text" className="border-0 py-3" />
 			</Form.Group>
+
 			<Form.Group className="mb-3" controlId="email">
 				<Form.Label>Email address</Form.Label>
-				<Form.Control type="email" className="border-0" />
+				<Form.Control type="email" className="border-0 py-3" />
 			</Form.Group>
-			<Form.Group className="mb-3" controlId="website">
+
+			{/* <Form.Group className="mb-3" controlId="website">
 				<Form.Label>Website</Form.Label>
-				<Form.Control type="text" className="border-0" />
-			</Form.Group>
+				<Form.Control type="text" className="border-0 py-3" />
+			</Form.Group> */}
+
+			<div className="mb-3">
+				<Form.Label htmlFor="website">Website</Form.Label>
+				<InputGroup>
+					<InputGroup.Text
+						className="border-0 py-3 text-white d-none d-md-block"
+						style={{ backgroundColor: '#C7C7C7' }}
+					>
+						https://
+					</InputGroup.Text>
+					<FormControl id="website" className="border-0 py-3" />
+				</InputGroup>
+			</div>
+
 			<Form.Group className="mb-3" controlId="message">
 				<Form.Label>Message</Form.Label>
-				<Form.Control as="textarea" rows={3} className="border-0" />
+				<Form.Control as="textarea" rows={3} className="border-0 py-3" />
 			</Form.Group>
 
 			<div className="form-check p-0">
